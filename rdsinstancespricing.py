cecff7122ead9d996f81220e5521828c5a7e739d
Evgeny Gridasov <evgeny.gridasov@gmail.com>
2014-01-12 00:28:31 +1000

    Remove copyright

--- rdsinstancespricing.py
+++ rdsinstancespricing.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python
 #
 # Copyright (c) 2013 Evgeny Gridasov (evgeny.gridasov@gmail.com)
-# Copyright (c) 2013 ITOC Australia  http://itoc.com.au
 #
 # Permission is hereby granted, free of charge, to any person obtaining
 # a copy of this software and associated documentation files (the
