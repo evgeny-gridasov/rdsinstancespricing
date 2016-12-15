48202da3b1528b0a603d6ad225e16971b5eb1f13
Evgeny Gridasov <evgeny.gridasov@gmail.com>
2016-12-15 16:35:58 +1100

    Added ca-central-1 and eu-west-2 regions

--- rdsinstancespricing.py
+++ rdsinstancespricing.py
@@ -35,14 +35,16 @@ RDS_REGIONS = [
 	"us-east-2",
 	"us-west-1",
 	"us-gov-west-1",
+	"ca-central-1",
 	"us-west-2",
 	"eu-west-1",
+	"eu-west-2",
 	"eu-central-1",
 	"ap-southeast-1",
 	"ap-southeast-2",
 	"ap-northeast-1",
 	"ap-northeast-2",
-	"ap-sout-1",
+	"ap-south-1",
 	"sa-east-1"
 ]
 
@@ -92,8 +94,10 @@ JSON_NAME_TO_RDS_REGIONS_API = {
 	"us-west-1" : "us-west-1",
 	"us-gov-west-1" : "us-gov-west-1",
 	"us-west-2" : "us-west-2",
+	"ca-central-1" : "ca-central-1",
 	"eu-ireland" : "eu-west-1",
 	"eu-west-1" : "eu-west-1",
+	"eu-west-2" : "eu-west-2",
 	"eu-frankfurt" : "eu-central-1",
 	"eu-central-1" : "eu-central-1",
 	"apac-sin" : "ap-southeast-1",
